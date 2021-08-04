635c68bd793a8fec27ae88afa2f25917aa0b8679
prateekLambda <prateeks@lambdatest.com>
2021-08-04 22:59:11 +0530

    Merge remote-tracking branch 'origin/master' into master
    # Conflicts:
    #	Edge.xml
    #	HighSierra.xml
    #	Mac.xml
    #	Sierra.xml
    #	Win10.xml
    #	devices.xml
    #	iOS.xml
    #	pom.xml
    #	porject.bat
    #	win7.xml
    #	win8.1.xml
    #	win8.xml

--- src/main/java/magicleapTesting/magicLeap.java
+++ src/main/java/magicleapTesting/magicLeap.java
@@ -89,7 +89,7 @@ public class magicLeap {
             for (int j = 0; j < 10; j++) {
                 try {
                     String[] file = {"5mb.jpg", "10MB1.jpg", "10MB2.jpg", "10MB3.jpg", "10MB4.jpg", "10MB5.jpg", "10MB6.jpg", "10MB7.jpg", "10MB8.jpg", "10MB9.jpg", "10MB10.jpg"};
-                    String region = "us";
+                    String region = "eu";
                     DesiredCapabilities capabilities = new DesiredCapabilities();
                     capabilities.setCapability("browserName", this.BrowserValue);
                     //   capabilities.setCapability("version", "latest");
