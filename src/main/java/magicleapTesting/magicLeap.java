7c02631c453d53d45e7af638fb2d2dfd5c39f0b1
prateekLambda <prateeks@lambdatest.com>
2021-08-23 15:09:43 +0530

    Merge remote-tracking branch 'origin/master' into master
    # Conflicts:
    #	Edge.xml
    #	HighSierra.xml
    #	Mac.xml
    #	Sierra.xml
    #	Win10.xml
    #	devices.xml
    #	iOS.xml
    #	pom.xml
    #	porject.bat
    #	win7.xml
    #	win8.1.xml
    #	win8.xml

--- src/main/java/magicleapTesting/magicLeap.java
+++ src/main/java/magicleapTesting/magicLeap.java
@@ -79,7 +79,7 @@ public class magicLeap {
         DateFormat formatter = new SimpleDateFormat("dd-MM-yyyy HH");
         formatter.setTimeZone(TimeZone.getTimeZone("Asia/Kolkata"));
         date = new Date();
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 5; i++) {
             for (int j = 0; j < 5; j++) {
                 try {
                     String[] file = {"5mb.jpg", "10MB1.jpg", "10MB2.jpg", "10MB3.jpg", "10MB4.jpg", "10MB5.jpg", "10MB6.jpg", "10MB7.jpg", "10MB8.jpg", "10MB9.jpg", "10MB10.jpg"};
