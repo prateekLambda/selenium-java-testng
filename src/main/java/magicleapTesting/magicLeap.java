fece42f4b55c5c326955c6ace89e76d0349f35a2
prateekLambda <prateeks@lambdatest.com>
2021-08-23 22:14:45 +0530

    Merge remote-tracking branch 'origin/master' into master
    # Conflicts:
    #	Edge.xml
    #	HighSierra.xml
    #	Mac.xml
    #	Sierra.xml
    #	Win10.xml
    #	devices.xml
    #	iOS.xml
    #	pom.xml
    #	porject.bat
    #	win7.xml
    #	win8.1.xml
    #	win8.xml

--- src/main/java/magicleapTesting/magicLeap.java
+++ src/main/java/magicleapTesting/magicLeap.java
@@ -203,9 +203,9 @@ public class magicLeap {
             System.out.println("==================TestStart+++++++++++++" + session + "++++++++++++++++TestStart==================");
             driver.get("https://www.amazon.in/");
             System.out.println(driver.getTitle());
-            for (int index = 0; index <= 50; index++) {
+           /* for (int index = 0; index <= 50; index++) {
                 Thread.sleep(1000);
-            }
+            }*/
           /*  driver.get("https://www.amazon.in/");
             System.out.println(driver.getTitle());
             for (int index = 0; index <= 20; index++) {
